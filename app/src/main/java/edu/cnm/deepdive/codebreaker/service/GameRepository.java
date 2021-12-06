5cc857ac688230b7ae9844e9ecc486b25929b243
Royce Reano <reano.royce@gmail.com>
2021-12-06 16:40:53 -0700

    In progress for displaying the ranking.

--- app/src/main/java/edu/cnm/deepdive/codebreaker/service/GameRepository.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/service/GameRepository.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
 import edu.cnm.deepdive.codebreaker.model.dao.GameDao;
 import edu.cnm.deepdive.codebreaker.model.dao.GuessDao;
+import edu.cnm.deepdive.codebreaker.model.dto.RankedUser;
 import edu.cnm.deepdive.codebreaker.model.entity.Game;
 import edu.cnm.deepdive.codebreaker.model.entity.Guess;
 import edu.cnm.deepdive.codebreaker.model.pojo.GameWithGuesses;
@@ -12,6 +13,7 @@ import io.reactivex.Single;
 import io.reactivex.schedulers.Schedulers;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Locale;
 
 public class GameRepository {
 
@@ -93,4 +95,14 @@ public class GameRepository {
         : Single.just(game);
   }
 
+  public Single<List<RankedUser>> getRankings(int length, int poolSize, RankingOrder order) {
+    return signInRepository
+        .refreshBearerToken()
+        .flatMap((token) ->
+            proxy.getRankings(length, poolSize,order.toString().toLowerCase(), token))
+        .subscribeOn(Schedulers.io());
+  }
+  public enum RankingOrder {
+    COUNT, TIME
+  }
 }
